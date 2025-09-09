bec01914980e8681c72f72e357a32881c59be6e2
MaguiMaluff <maguimalu4@gmail.com>
2025-09-09 18:40:27 -0300

    First Commit

---
+++ src/main/java/com/example/haxagonal/hexagonal_basico/product/domain/ports/out/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.example.haxagonal.hexagonal_basico.product.domain.ports.out;
+
+import com.example.haxagonal.hexagonal_basico.product.domain.model.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+
+    Product save(Product product);
+    Optional<Product> findById(long id);
+    List<Product> findAll();
+    Optional<Product> update(Long id, Product product);
+    boolean deleteById(long id);
+
+}
---
+++ src/main/java/com/example/haxagonal/hexagonal_basico/product/infrastructure/web/dto/ProductRequest.java
@@ -0,0 +1,21 @@
+package com.example.haxagonal.hexagonal_basico.product.infrastructure.web.dto;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Data
+public class ProductRequest {
+
+    @NotBlank(message = "Description is mandatory")
+    private String description;
+
+    @NotNull(message = "Price is mandatory")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than 0")
+    private BigDecimal price;
+
+}
